5bce6588154f1475a8d210da0cb02fcef6bd2ae2
Ryan Chaffee <rchaff7@yahoo.com>
2020-09-18 09:34:08 -0500

    classes

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,6 +1,11 @@
 import { Payload } from './Payload';
 
 export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: object[] = [];
+    astronauts: object[] = [];
     
+
 
 }
